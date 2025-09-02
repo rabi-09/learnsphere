dfbb487573d3c07537d12441ec2afba1d81c1be2
rabi.09 <dasrabi2004@gmail.com>
2025-09-03 01:11:14 +0530

    Project started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Hero from './sections/Hero'
+import { Navigation } from './sections/Navigation'
+
+function App() {
+  return (
+    <>
+    <Navigation />
+    <Hero/>
+    </>
+  )
+}
+
+export default App
